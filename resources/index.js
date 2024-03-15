b660c5e5d405faa6e521392df10c3892b53ee9f2
Niharika Khurana <khurananiharika96@gmail.com>
2024-03-15 01:13:30 -0400

    added saving chart js

--- resources/index.js
+++ resources/index.js
@@ -393,9 +393,9 @@ const calculateOptions = (userValues) => {
 }
 
 /**
- * The `calculate` function retrieves data from a table, validates it, and then performs calculations
- * based on the data.
- */
+* The `calculate` function retrieves data from a table, validates it, and then performs calculations
+* based on the data.
+*/
 const calculate = () => {
     const loader = document.getElementById("loader-wrapper");
     loader.style.display = 'flex';
@@ -445,15 +445,15 @@ const calculate = () => {
 }
 
 /**
- * The function `addGreenWizardContainer` creates a container element with a result text and a dropdown
- * menu, and appends it to a specific element on the page.
- * @param resultText - The `resultText` parameter is a string that represents the text to be displayed
- * in the result element of the green wizard container.
- * @param options - An array of options for the dropdown menu. Each option should be a string.
- * @param index - The `index` parameter is used to uniquely identify each green wizard container. It is
- * typically used when creating the `id` attribute for the select element, as well as when creating the
- * `for` attribute for the result element.
- */
+* The function `addGreenWizardContainer` creates a container element with a result text and a dropdown
+* menu, and appends it to a specific element on the page.
+* @param resultText - The `resultText` parameter is a string that represents the text to be displayed
+* in the result element of the green wizard container.
+* @param options - An array of options for the dropdown menu. Each option should be a string.
+* @param index - The `index` parameter is used to uniquely identify each green wizard container. It is
+* typically used when creating the `id` attribute for the select element, as well as when creating the
+* `for` attribute for the result element.
+*/
 const addGreenWizardContainer = (resultText, options, index) => {
     var container = document.createElement('div');
     container.classList.add('green-wizard-container');
@@ -489,11 +489,11 @@ const addGreenWizardContainer = (resultText, options, index) => {
 }
 
 /**
- * The function `getDescriptionValue` returns the value of the first property in an object that starts
- * with the string "description".
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'description' in the given object.
- */
+* The function `getDescriptionValue` returns the value of the first property in an object that starts
+* with the string "description".
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'description' in the given object.
+*/
 const getDescriptionValue = (value) => {
     for (const property in value) {
         if (property.startsWith('description')) {
@@ -504,11 +504,11 @@ const getDescriptionValue = (value) => {
 
 
 /**
- * The function `getTypeValue` returns the value of the property that starts with 'type' in the given
- * object.
- * @param value - The `value` parameter is the object that we want to extract the value from.
- * @returns The value of the property that starts with 'type' in the given object.
- */
+* The function `getTypeValue` returns the value of the property that starts with 'type' in the given
+* object.
+* @param value - The `value` parameter is the object that we want to extract the value from.
+* @returns The value of the property that starts with 'type' in the given object.
+*/
 const getTypeValue = (value) => {
     for (const property in value) {
         if (property.startsWith('type')) {
@@ -519,11 +519,11 @@ const getTypeValue = (value) => {
 
 
 /**
- * The function `getYearValue` returns the value of the first property in an object that starts with
- * the word "year".
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'year' in the given object.
- */
+* The function `getYearValue` returns the value of the first property in an object that starts with
+* the word "year".
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'year' in the given object.
+*/
 const getYearValue = (value) => {
     for (const property in value) {
         if (property.startsWith('year')) {
@@ -534,11 +534,11 @@ const getYearValue = (value) => {
 
 
 /**
- * The function `getMakeValue` returns the value of the first property in an object that starts with
- * the string "make".
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'make' in the given object.
- */
+* The function `getMakeValue` returns the value of the first property in an object that starts with
+* the string "make".
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'make' in the given object.
+*/
 const getMakeValue = (value) => {
     for (const property in value) {
         if (property.startsWith('make')) {
@@ -547,11 +547,11 @@ const getMakeValue = (value) => {
     }
 }
 /**
- * The function `getModelValue` returns the value of the first property in an object that starts with
- * the string "model".
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'model' is being returned.
- */
+* The function `getModelValue` returns the value of the first property in an object that starts with
+* the string "model".
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'model' is being returned.
+*/
 
 const getModelValue = (value) => {
     for (const property in value) {
@@ -562,11 +562,11 @@ const getModelValue = (value) => {
 }
 
 /**
- * The function `getFlexFuelValue` returns the value of the first property in an object that starts
- * with 'flex-fuel'.
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'flex-fuel' is being returned.
- */
+* The function `getFlexFuelValue` returns the value of the first property in an object that starts
+* with 'flex-fuel'.
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'flex-fuel' is being returned.
+*/
 const getFlexFuelValue = (value) => {
     for (const property in value) {
         if (property.startsWith('flex-fuel')) {
@@ -576,11 +576,11 @@ const getFlexFuelValue = (value) => {
 }
 
 /**
- * The function `getFuelTypeValue` returns the value of the property that starts with 'fuel-type' from
- * the given object.
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'fuel-type' is being returned.
- */
+* The function `getFuelTypeValue` returns the value of the property that starts with 'fuel-type' from
+* the given object.
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'fuel-type' is being returned.
+*/
 const getFuelTypeValue = (value) => {
     for (const property in value) {
         if (property.startsWith('fuel-type')) {
@@ -590,11 +590,11 @@ const getFuelTypeValue = (value) => {
 }
 
 /**
- * The function `getAnnualFuelValue` returns the value of the first property in the given object that
- * starts with "annual-fuel".
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'annual-fuel' is being returned.
- */
+* The function `getAnnualFuelValue` returns the value of the first property in the given object that
+* starts with "annual-fuel".
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'annual-fuel' is being returned.
+*/
 const getAnnualFuelValue = (value) => {
     for (const property in value) {
         if (property.startsWith('annual-fuel')) {
@@ -604,11 +604,11 @@ const getAnnualFuelValue = (value) => {
 }
 
 /**
- * The function `getAnnualKmValue` returns the value of the first property in the `value` object that
- * starts with "annual-vkt".
- * @param value - The `value` parameter is an object that contains properties.
- * @returns The value of the property that starts with 'annual-vkt' is being returned.
- */
+* The function `getAnnualKmValue` returns the value of the first property in the `value` object that
+* starts with "annual-vkt".
+* @param value - The `value` parameter is an object that contains properties.
+* @returns The value of the property that starts with 'annual-vkt' is being returned.
+*/
 const getAnnualKmValue = (value) => {
     for (const property in value) {
         if (property.startsWith('annual-vkt')) {
@@ -722,8 +722,8 @@ const showEmissions = () => {
 
     calculateTotalEmissions(currentValues);
     document.querySelector('.emission-container').classList.remove('display-none');
-    document.querySelector('.emission-bar-graphs').scrollIntoView();
     document.querySelector('.btn-saving-result').classList.remove('display-none');
+    document.querySelector('.btn-saving-result').scrollIntoView();
 }
 
 const calculateEmissionSavings = (dropdownValue, emissionsIntensity, annualEmission) => {
@@ -744,6 +744,10 @@ const calculateEmissionSavings = (dropdownValue, emissionsIntensity, annualEmiss
             const avgICEintensityDatabase = 3;
             percentageSavings = (emissionsIntensity - avgICEintensityDatabase) / emissionsIntensity * 100;
             totalEmissionSavings = ((annualEmission * percentageSavings) / 1000000).toFixed(2);
+            percentageSavings = Math.round(percentageSavings);
+            const lowerBound = percentageSavings - 1;
+            const upperBound = percentageSavings + 1;
+            percentageSavings = `${lowerBound}-${upperBound}%`;
             return {
                 totalEmissionSavings,
                 percentageSavings
@@ -771,6 +775,10 @@ const calculateEmissionSavings = (dropdownValue, emissionsIntensity, annualEmiss
     const evEmissionIntensity = electricalEfficiency / 100 * coefficientValue;
     const intialpercentageSavings = (emissionsIntensity - evEmissionIntensity) / emissionsIntensity;
     percentageSavings = intialpercentageSavings * 100;
+    percentageSavings = Math.round(percentageSavings);
+    const lowerBound = percentageSavings - 1;
+    const upperBound = percentageSavings + 1;
+    percentageSavings = `${lowerBound}-${upperBound}%`;
     totalEmissionSavings = ((intialpercentageSavings * annualEmission) / 1000000).toFixed(2);
     const values = {
         totalEmissionSavings,
@@ -799,28 +807,32 @@ function createComparisonGraph(emissionsIntensity) {
     // Create dummy data for comparison (replace this with actual data)
     var comparisonLabels = ['Existing Vehicle', 'Vehicle 2', 'Vehicle 3', 'Vehicle 4', 'Vehicle 5', 'Vehicle 6', 'Vehicle 7', 'Vehicle 8', 'Vehicle 9', 'Vehicle 10'];
     var comparisonData = [selectedVehicleEmissionsIntensity, 2.5, 3.0, 3.2, 2.8, 3.5, 2.9, 3.1, 2.7, 3.3];
-    // Display the comparison graph
-    var comparisonGraphCtx = document.getElementById('comparisonGraph').getContext('2d');
-    var comparisonGraph = new Chart(comparisonGraphCtx, {
-        type: 'bar',
-        data: {
-            labels: comparisonLabels,
-            datasets: [{
-                label: 'Emissions Intensity Comparison (gCO2e/km)',
-                data: comparisonData,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    const vehicleData = [
+        {
+            x: comparisonLabels,
+            y: comparisonData,
+            type: 'bar',
+            marker: {
+                color: 'rgba(75, 192, 192, 0.2)',
+                line: {
+                    color: '#rgba(75, 192, 192, 1)',
+                    width: 1.5
                 }
-            }
+            },
+            width: Array.from({ length: comparisonData.length }, () => 0.5),
+            bargap: 0.2,
         }
-    });
+    ];
+    const vehiclelayout = {
+        title: 'Emissions Intensity Comparison (gCO2e/km)',
+        font: {
+            color: '#5caaff',
+            size: 16
+        },
+        width: 700,
+    }
+
+    Plotly.newPlot('comparisonGraph', vehicleData, vehiclelayout, { displaylogo: false });
 }
 
 const showSavings = () => {
@@ -836,7 +848,7 @@ const showSavings = () => {
         const emissionValues = calculateEmissionSavings(dropdownValue, emissionsIntensity, annualEmission);
         data.push({ fleetVehicle: resultText, actionApplied: dropdownValue, emissionSavings: emissionValues.totalEmissionSavings, savingsPercentage: emissionValues.percentageSavings });
         if (dropdownValue === 'Right Size to Car')
-            createComparisonGraph(index, emissionsIntensity);
+            createComparisonGraph(emissionsIntensity);
     });
 
     var parentElement = document.querySelector('.action-savings-wizard');
@@ -868,5 +880,11 @@ const showSavings = () => {
         containerBody.appendChild(savingsPercentageDiv);
     });
     document.querySelector('.savings-chart').classList.remove('display-none');
-    document.querySelector('.savings-chart').scrollIntoView();
+    document.querySelector('.chart-container').classList.remove('display-none');
+    document.querySelector('.save-buttons').classList.remove('display-none');
+    document.querySelector('.save-buttons').classList.add('d-flex');
+    if (document.querySelector('.chart-container'))
+        document.querySelector('.chart-container').scrollIntoView();
+    else
+        document.querySelector('.savings-chart').scrollIntoView();
 }
